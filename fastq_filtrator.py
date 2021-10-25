d082ccee1257016cdd7a2acc74ebb8f13d05f862
IlonaGA <alexandragorbonos2000@gmail.com>
2021-10-25 10:50:36 +0300

    fix some bugs

--- fastq_filtrator.py
+++ fastq_filtrator.py
@@ -1,13 +1,10 @@
 def check_gc_bounds(line, bottom, top):
     length = len(line)
-    G_count = line.count('G')
-    C_count = line.count('C')
+    G_count = line.upper().count('G')
+    C_count = line.upper().count('C')
     percent = ((G_count + C_count) / length) * 100
 
-    if (percent < top) and (percent > bottom):
-        return True
-    else:
-        return False
+    return bottom <= percent <= top
 
 
 def check_length_bounds(line, bottom, top):
@@ -22,7 +19,7 @@ def check_quality_threshold(line, min_quality):
     summ = 0
     for char in line:
         summ += ord(char) - ord('!')
-    return summ / len(line) > min_quality
+    return summ / len(line) >= min_quality
 
 
 def main(input_fastq, output_file_prefix, gc_bounds=(0, 100),
@@ -36,32 +33,34 @@ def main(input_fastq, output_file_prefix, gc_bounds=(0, 100),
     fastq = open(input_fastq, 'r')
     passed = open(output_file_prefix + '_passed.fastq', 'w+')
     failed = open(output_file_prefix + '_failed.fastq', 'w+')
-    file = []
 
-    for line in fastq:
-        file.append(line)
+    with open(input_fastq, 'r') as fastq, \
+         open(output_file_prefix + '_passed.fastq', 'w+') as passed, \
+         open(output_file_prefix + '_failed.fastq', 'w+') as failed:
 
-    for i in range(0, len(file), 4):
-        if check_gc_bounds(file[i + 1],
-                           gc_bounds[0],
-                           gc_bounds[1]) and \
-            check_length_bounds(file[i + 1],
-                                length_bounds[0],
-                                length_bounds[1]) and \
-            check_quality_threshold(file[i + 1],
-                                    quality_threshold):
+        file = []
 
-            passed.write(file[i])
-            passed.write(file[i + 1])
-            passed.write(file[i + 2])
-            passed.write(file[i + 3])
+        for line in fastq:
+            file.append(line[:-1])
 
-        elif save_filtered:
-            failed.write(file[i])
-            failed.write(file[i + 1])
-            failed.write(file[i + 2])
-            failed.write(file[i + 3])
+        for i in range(0, len(file), 4):
+            if check_gc_bounds(file[i + 1],
+                               gc_bounds[0],
+                               gc_bounds[1]) and \
+               check_length_bounds(file[i + 1],
+                                   length_bounds[0],
+                                   length_bounds[1]) and \
+               check_quality_threshold(file[i + 1],
+                                       quality_threshold):
 
-    fastq.close()
-    passed.close()
-    failed.close()
+                passed.write(file[i] + '\n')
+                passed.write(file[i + 1] + '\n')
+                passed.write(file[i + 2] + '\n')
+                passed.write(file[i + 3] + '\n')
+
+            elif save_filtered:
+                failed.write(file[i] + '\n')
+                failed.write(file[i + 1] + '\n')
+                failed.write(file[i + 2] + '\n')
+                failed.write(file[i + 3] + '\n')
+
