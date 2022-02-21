476a711c77256c3d4b5b2e6cdeac0f1441b323ad
Alexandra <60537387+IlonaGA@users.noreply.github.com>
2022-02-22 00:15:11 +0200

    Update Pandas.py

--- Pandas.py
+++ Pandas.py
@@ -2,18 +2,19 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-#1
+# 1
 df = pd.read_csv('train.csv')
 colors = ['salmon', 'plum', 'purple', 'cornflowerblue']
 plt.hist(df[['A', 'C', 'T', 'G']], color=colors, label=['A', 'C', 'T', 'G'])
 plt.legend()
 plt.show()
 
-#2
+# 2
 columns = ['pos', 'reads_all', 'mismatches', 'deletions', 'insertions']
-df.loc[df['mismatches'] > df['mismatches'].mean(), columns].to_csv('train_part.csv', index=None)
+df.loc[df['mismatches'] > df['mismatches'].mean(), columns]\
+       .to_csv('train_part.csv', index=None)
 
-#3
+# 3
 marks_df = pd.read_excel('marks.xlsx', sheet_name='Python')
 names = marks_df.columns[7:]
 marks_df.loc[:, names].fillna(0, inplace=True)
